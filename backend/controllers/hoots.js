a1630f0d0e0b5a6b8b56411a453f45b24d3d4a75
galichinsky <galichinsky@gmail.com>
2024-09-11 13:05:45 -0500

    complete hoot backend

--- backend/controllers/hoots.js
+++ backend/controllers/hoots.js
@@ -23,6 +23,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET /api/:hootId - Return a single hoot
+router.get('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate('author');
+    res.status(200).json(hoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // POST /api/hoots - Create a new hoot
 router.post('/', async (req, res) => {
@@ -34,6 +43,95 @@ router.post('/', async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
+  }
+});
+
+// PUT /api/hoots/:hootId - Update a hoot
+router.put('/:hootId', async (req, res) => {
+  try {
+    // Find the hoot:
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    // Check permissions:
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    // Update hoot:
+    const updatedHoot = await Hoot.findByIdAndUpdate(
+      req.params.hootId,
+      req.body,
+      { new: true }
+    );
+
+    // Append req.user to the author property:
+    updatedHoot._doc.author = req.user;
+
+    // Issue JSON response:
+    res.status(200).json(updatedHoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// DELETE /api/hoots/:hootId - Delete a hoot
+router.delete('/:hootId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// POST /api/hoots/:hootId/comments - Create a new comment
+router.post('/:hootId/comments', async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.push(req.body);
+    await hoot.save();
+
+    // Find the newly created comment:
+    const newComment = hoot.comments[hoot.comments.length - 1];
+
+    newComment._doc.author = req.user;
+
+    // Respond with the newComment:
+    res.status(201).json(newComment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// UPDATE /api/hoots/:hootId/comments/:commentId - Update a comment
+router.put('/:hootId/comments/:commentId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    const comment = hoot.comments.id(req.params.commentId);
+    comment.text = req.body.text;
+    await hoot.save();
+    res.status(200).json({ message: 'Ok' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE /api/hoots/:hootId/comments/:commentId - Delete a comment
+router.delete('/:hootId/comments/:commentId', async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.remove({ _id: req.params.commentId });
+    await hoot.save();
+    res.status(200).json({ message: 'Ok' });
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
