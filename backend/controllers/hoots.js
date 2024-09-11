479bbaebd3cf0d3e9976aadeb564f904bf0d93d0
galichinsky <galichinsky@gmail.com>
2024-09-11 12:25:26 -0500

    add index functionality

--- backend/controllers/hoots.js
+++ backend/controllers/hoots.js
@@ -11,6 +11,20 @@ const router = express.Router();
 
 router.use(verifyToken);
 
+// GET /api/hoots - Return all hoots
+router.get('/', async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate('author')
+      .sort({ createdAt: 'desc' });
+    res.status(200).json(hoots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+// POST /api/hoots - Create a new hoot
 router.post('/', async (req, res) => {
   try {
     req.body.author = req.user._id;
